6b2ccf72bd95493431b5cc6300804628bbfdfb96
gabsifuentes <gabsifuentesss@gmail.com>
2026-05-06 15:29:16 -0400

    método tranferir

--- SistemaBancarIo/src/Main.java
+++ SistemaBancarIo/src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	static ArrayList<Conta> contasBancarias = new ArrayList<>();
 	
 	//metodos da main
-	public void criarConta(String nome, int numConta, double saldo) {
+	public void CriarConta(String nome, int numConta, double saldo) {
 		if(verificaConta(numConta)) {
 			System.out.println("Já existe uma conta com esse ID! Tente novamente");
 		}else {
 			contasBancarias.add(new Conta(nome, numConta, saldo));
 		}
-	}
+	} 
 
 	public static boolean verificaConta(int numConta) {
 		//cria um objeto "temporario" de Conta que recebe o proximo item da lista pra verificar
