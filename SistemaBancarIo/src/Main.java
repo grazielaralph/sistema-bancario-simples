96bd1c1b6695f3f497c8b52353281509e9f2db21
Graziela Ralph <ralphgraziela25@gmail.com>
2026-05-06 15:38:38 -0400

    update main

--- SistemaBancarIo/src/Main.java
+++ SistemaBancarIo/src/Main.java
@@ -1,39 +1,4 @@
-import java.util.ArrayList;
-
 public class Main {
-	//"banco de dados" armazenando as contas
-	static ArrayList<Conta> contasBancarias = new ArrayList<>();
-	
-	//metodos da main
-	public void criarConta(String nome, int numConta, double saldo) {
-		if(verificaConta(numConta)) {
-			System.out.println("Já existe uma conta com esse ID! Tente novamente");
-		}else {
-			contasBancarias.add(new Conta(nome, numConta, saldo));
-		}
-	}
-
-	public static boolean verificaConta(int numConta) {
-		//cria um objeto "temporario" de Conta que recebe o proximo item da lista pra verificar
-		for (Conta c : contasBancarias) {
-			if(c.getNum_conta() == numConta) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public void sacar() {}
-	
-	public void depositar() {}
-	
-	public void transferir() {}
-	
-	public void consultarConta(int numConta) {
-		if(verificaConta(numConta)) {
-			
-		}
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
